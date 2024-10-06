0395cd54282c5496a2f9ab3e242399e32ab703ca
Leonardo Ramos <leoamorimr@gmail.com>
2024-10-06 17:40:59 +0100

    fix: test coverage

--- src/repository/prisma/prisma-rent.repository.spec.ts
+++ src/repository/prisma/prisma-rent.repository.spec.ts
@@ -34,6 +34,30 @@ describe("PrismaRentRepository", () => {
     });
   });
 
+  describe("create", () => {
+    it("should create a rent entity", async () => {
+      (prismaService.rent.create as jest.Mock).mockResolvedValue(
+        fakeRentEntity,
+      );
+
+      const result = await repository.create(fakeRentEntity);
+
+      expect(prismaService.rent.create).toHaveBeenCalledWith({
+        data: {
+          id: expect.any(String),
+          lockerId: fakeRentEntity.lockerId,
+          weight: fakeRentEntity.weight,
+          size: fakeRentEntity.size,
+          status: fakeRentEntity.status,
+        },
+        include: {
+          locker: true,
+        },
+      });
+      expect(result).toEqual(fakeRentEntity);
+    });
+  });
+
   describe("exists", () => {
     it("should return true if rent exists", async () => {
       prismaService.rent.findUnique = jest
@@ -98,6 +122,29 @@ describe("PrismaRentRepository", () => {
       const result = await repository.findAll();
       expect(result).toEqual(rentList);
       expect(prismaService.rent.findMany).toHaveBeenCalled();
+    });
+
+    describe("findOneOrThrow", () => {
+      it("should return a rent entity if found", async () => {
+        prismaService.rent.findUniqueOrThrow = jest.fn().mockResolvedValue(fakeRentEntity);
+
+        const result = await repository.findOneOrThrow(fakeUUID);
+        expect(result).toEqual(fakeRentEntity);
+        expect(prismaService.rent.findUniqueOrThrow).toHaveBeenCalledWith({
+          where: { id: fakeUUID },
+          include: { locker: true },
+        });
+      });
+
+      it("should throw an error if rent is not found", async () => {
+        prismaService.rent.findUniqueOrThrow = jest.fn().mockRejectedValue(new Error("Rent not found"));
+
+        await expect(repository.findOneOrThrow(fakeUUID)).rejects.toThrow("Rent not found");
+        expect(prismaService.rent.findUniqueOrThrow).toHaveBeenCalledWith({
+          where: { id: fakeUUID },
+          include: { locker: true },
+        });
+      });
     });
   });
 });
